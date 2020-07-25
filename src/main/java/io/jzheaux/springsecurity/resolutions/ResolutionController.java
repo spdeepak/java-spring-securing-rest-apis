00ad037a82f0698f9a7caeba86714b431db2e94d
spdeepak <spdeepak@outlook.com>
2020-07-25 10:36:10 +0200

    3.2

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -1,5 +1,6 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,30 +21,35 @@ public class ResolutionController {
     }
 
     @GetMapping("/resolutions")
+    @PreAuthorize("hasAuthority('resolution:read')")
     public Iterable<Resolution> read() {
         return this.resolutions.findAll();
     }
 
     @GetMapping("/resolution/{id}")
+    @PreAuthorize("hasAuthority('resolution:read')")
     public Optional<Resolution> read(@PathVariable("id") UUID id) {
         return this.resolutions.findById(id);
     }
 
     @PostMapping("/resolution")
+    @PreAuthorize("hasAuthority('resolution:write')")
     public Resolution make(@CurrentUsername String username, @RequestBody String text) {
         Resolution resolution = new Resolution(text, username);
         return this.resolutions.save(resolution);
     }
 
-    @PutMapping(path = "/resolution/{id}/revise")
     @Transactional
+    @PutMapping(path = "/resolution/{id}/revise")
+    @PreAuthorize("hasAuthority('resolution:write')")
     public Optional<Resolution> revise(@PathVariable("id") UUID id, @RequestBody String text) {
         this.resolutions.revise(id, text);
         return read(id);
     }
 
-    @PutMapping("/resolution/{id}/complete")
     @Transactional
+    @PutMapping("/resolution/{id}/complete")
+    @PreAuthorize("hasAuthority('resolution:write')")
     public Optional<Resolution> complete(@PathVariable("id") UUID id) {
         this.resolutions.complete(id);
         return read(id);
