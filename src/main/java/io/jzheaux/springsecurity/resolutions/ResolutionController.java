f68bbc27c72efb74125d00dcb863a2e133cd6f4f
spdeepak <spdeepak@outlook.com>
2020-07-26 09:28:58 +0200

    4.4

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -23,7 +23,7 @@ public class ResolutionController {
         this.resolutions = resolutions;
     }
 
-    @CrossOrigin//(maxAge = 0) if locally verifying
+    @CrossOrigin(allowCredentials = "true")//(maxAge = 0) if locally verifying
     @GetMapping("/resolutions")
     @PreAuthorize("hasAuthority('resolution:read')")
     @PostFilter("@post.filter(#root)")
